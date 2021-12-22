9dbe899953b26644ad249d5a93f7f08a319cab05
masaya.maruyama <yayewanshan2@gmail.com>
2021-12-22 20:02:22 +0900

    2021.12.22

--- SlideshowApp/ResultViewController.swift
+++ SlideshowApp/ResultViewController.swift
@@ -11,26 +11,33 @@ class ResultViewController: UIViewController {
     //UIImage Outlet 接続
     @IBOutlet weak var photo2: UIImageView!
     
-    
+    var x:Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         //UIImage表示
-        photo2.image = UIImage (named: "ra-0")
+        let imageArray:[UIImage] = [
+            UIImage(named: "ra-0")!,
+            UIImage(named: "cha-1")!,
+            UIImage(named: "gyo-2")!
+        ]
+        
+        photo2.image = imageArray[x]
+        
         
     }
     
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var photo1: UIImageView!
     var photoNumber = 0
     
@@ -23,22 +23,27 @@ class ViewController: UIViewController {
     //画像をタップすると画面を遷移
     @IBOutlet var tapGes: UITapGestureRecognizer!
     func tapAction(_ sender: Any) {
-        }
+    }
     
     var imageArray:[UIImage] = [
-           UIImage(named: "ra-0")!,
-           UIImage(named: "cha-1")!,
-           UIImage(named: "gyo-2")!
-]
+        UIImage(named: "ra-0")!,
+        UIImage(named: "cha-1")!,
+        UIImage(named: "gyo-2")!
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         photo1.image = imageArray[0]
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        resultViewController.x = photoNumber
+    }
+    
     @IBAction func forwardButton(_ sender: UIButton) {
-        if photoNumber == (imageArray.count - 1){
-            photoNumber -= (imageArray.count - 1)
+        if photoNumber == (imageArray.count - 1){ //Max
+            photoNumber = 0
         }else{
             photoNumber += 1
         }
@@ -56,35 +61,34 @@ class ViewController: UIViewController {
     //playButton
     @IBAction func playButton(_ sender: Any) {
         // 再生中か停止しているかを判定
-               if (timer == nil) {
-                   timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(changeImage), userInfo: nil, repeats: true)
-                   playButton.setTitle("stop", for: .normal)
-                //button無効
-                (backButton.isEnabled,backButton.isHidden) = (false,true)
-                (forwardButton.isEnabled,forwardButton.isHidden) = (false,true)
-                tapGes.isEnabled = false
-               } else {
-                   timer.invalidate()
-                   timer = nil
-                   playButton.setTitle("play", for: .normal)
-                //button有効
-                ( backButton.isEnabled,forwardButton.isEnabled,tapGes.isEnabled ) = ( true , true , true )
-                ( backButton.isHidden,forwardButton.isHidden ) = ( false , false )
-               }
-           }
-
-           @objc func changeImage() {
-            photoNumber += 1
-               // photoNumber.0〜max.repeat
-               if (photoNumber == imageArray.count) {
-                photoNumber = 0
-               }
-               // photoNumberの画像を表示
-               photo1.image = imageArray[photoNumber]
-           }
-        
-//帰りのsegue
-@IBAction func unwind(_ segue:UIStoryboardSegue) {
+        if (timer == nil) {
+            timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(changeImage), userInfo: nil, repeats: true)
+            playButton.setTitle("stop", for: .normal)
+            //button無効
+            (backButton.isEnabled,backButton.isHidden) = (false,true)
+            (forwardButton.isEnabled,forwardButton.isHidden) = (false,true)
+        } else {
+            timer.invalidate()
+            timer = nil
+            playButton.setTitle("play", for: .normal)
+            //button有効
+            ( backButton.isEnabled,forwardButton.isEnabled,tapGes.isEnabled ) = ( true , true , true )
+            ( backButton.isHidden,forwardButton.isHidden ) = ( false , false )
+        }
     }
-
+    
+    @objc func changeImage() {
+        photoNumber += 1
+        // photoNumber.0〜max.repeat
+        if (photoNumber == imageArray.count) {
+            photoNumber = 0
+        }
+        // photoNumberの画像を表示
+        photo1.image = imageArray[photoNumber]
+    }
+    
+    //帰りのsegue
+    @IBAction func unwind(_ segue:UIStoryboardSegue) {
+    }
+    
 }
